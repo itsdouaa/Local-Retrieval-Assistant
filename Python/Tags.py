22d44638338b4192bad19f45578ed4d8aad3b25a
Douaa_Meriem_BELANOUAR <155255716+itsdouaa@users.noreply.github.com>
2025-09-04 21:52:17 +0100

    Add files via upload

--- Python/Tags.py
+++ Python/Tags.py
@@ -1,17 +1,31 @@
+import os
 import re
 import ast
 from collections import Counter
+import subprocess
 
 def load_stopwords(path):
     with open(path, "r", encoding="utf-8") as f:
-        return set(ast.literal_eval(f.read()))
+        return set(word.strip() for word in f if word.strip())
 
 def get(text):
     words = re.findall(r'\b\w+\b', text.lower())
 
-    stopwords = load_stopwords("/home/douaa/assistant/stopwords.txt")
+    path = subprocess.run(
+        ["find", os.path.expanduser("~"), "-name", "stopwords.txt"],
+        check=True,
+        text=True,
+        capture_output=True
+    )
 
-    filtered_words = [word for word in words if word not in french_stopwords and word not in english_stopwords and word not in arabic_stopwords and len(word) > 3]
+    try:
+        path_ = path.stdout.strip().splitlines()[0]
+    except IndexError:
+        raise RuntimeError("Stopwords file not found !")
+
+    stopwords = load_stopwords(path_)
+
+    filtered_words = [word for word in words if word not in stopwords and len(word) > 3]
     common_words = Counter(filtered_words).most_common(20)
 
     return [word for word, count in common_words]
