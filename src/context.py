24133592d55eb4c75db7881d0db7eb7a5af8eb30
Douaa Meriem BELANOUAR <meriemdouaa514@gmail.com>
2025-11-21 18:33:15 +0100

    Add files via upload
    just completed!, it was like empty before

--- src/context.py
+++ src/context.py
@@ -1,6 +1,24 @@
 import file
+import embeddings
+import history
+from database import Database, Virtual_Table 
 
-def retrieve(question):
-    context = []
-    
+def retrieve(question, opened_db: Database = None):
+    context = ""
+    if opened_db:
+        question_embeddings = embeddings.generate(question)
+        retrieved_results = opened_db.get_table("embeddings").search_similar([question_embeddings.tobytes()])
+        #prochainement traitement de résultats
+        #retrieved_text = ...
+    added_context = query()
+    context += added_context #+ retrieved_text
     return context
+    
+def query():
+    added_context: str = ""
+    add_file = {"yes": file.load, "y": file.load}
+    add = add_file.get(input("do you want to add some context/files ?\n").lower())
+    while add:
+        added_context += add()
+        add = add_file.get(input("do you want to add some context/files ?\n").lower())
+    return added_context
