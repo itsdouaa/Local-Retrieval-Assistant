38cb6260446a0289ccdb3867070359da2216b826
Douaa Meriem BELANOUAR <meriemdouaa514@gmail.com>
2025-11-26 21:45:01 +0100

    Add files via upload
    j'ai fini la fonction retrieve elle était incomplète, mais j'ai retiré la fonction query() qui appelle file.load(), parce qu'au plus tard  avec l'interface elle aura un boutton spécifique!

--- src/context.py
+++ src/context.py
@@ -1,4 +1,3 @@
-import file
 import embeddings
 import history
 from database import Database, Virtual_Table 
@@ -8,22 +7,24 @@ attempt = Attempt()
 
 def retrieve(question, opened_db: Database = None):
     context = ""
-    if opened_db:
-        question_embeddings = embeddings.generate(question)
-        retrieved_results = opened_db.get_table("embeddings").search_similar([question_embeddings.tobytes()])
-        #prochainement traitement de résultats
-        #retrieved_text = ...
-    added_context = query()
-    context += added_context #+ retrieved_text
-    return context
+    if not opened_db:
+        return context
+        
+    question_embeddings = embeddings.generate(question)
+    retrieved_results = opened_db.get_table("embeddings").search_similar([question_embeddings])
+    if retrieved_results:
+        messages_ids = []
+        for result in retrieved_results:
+            if opened_db.get_table("embeddings_message").select(["message_id"], "embedding_rowid == ?", [result]):
+                messages_ids.append(opened_db.get_table("embeddings_message").select(["message_id"], "embedding_rowid == ?", [result])[0][0])
+        for _id in messages_ids:
+            message = opened_db.get_table("history").select(["content"], "id = ?", [_id])[0][0]
+            print(message)
+            context += message + "\n\n"
+    #except Exception as e:
+        #print(f"Erreur lors de la récupération: {e}")
+        #context = ""
     
-def query():
-    added_context: str = ""
-    add_file = {"yes": file.load, "y": file.load}
-    add = add_file.get(attempt.safe_input("do you want to add some context/files ?").strip().lower())
-    attempt.reset()
-    while add and attempt.should_retry():
-        attempt.increment()
-        added_context += add()
-        add = add_file.get(attempt.safe_input("do you want to add some context/files ?").strip().lower())
-    return added_context
+    print(f"\n\n context : \n{context}\n\n")
+    return context.strip()
+
